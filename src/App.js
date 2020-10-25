1383c12d8e440fb46fc0e763569a365c2a3bc01d
plpetkov-tech <plpetkov@tutanota.com>
2020-10-25 14:03:41 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import man from './man.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={man} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Plame you have to edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
